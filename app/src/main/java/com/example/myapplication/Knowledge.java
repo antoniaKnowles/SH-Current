8ecc1607457ab5406e75f5a4fc5c74302beef0f7
antoniaKnowles <antoniabackupmac@gmail.com>
2021-03-12 09:52:38 +0000

    mar 12 10am

---
+++ app/src/main/java/com/example/myapplication/Knowledge.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Knowledge extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_knowledge, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.floatingActionButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                NavHostFragment.findNavController(Knowledge.this)
+                        .navigate(R.id.action_knowledge_to_FirstFragment);
+
+
+            }
+        });
+
+
+
+    }
+}

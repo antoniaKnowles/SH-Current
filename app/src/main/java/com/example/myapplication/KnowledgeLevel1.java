c0c8da0588557f05bbb60daf2ccf7201b902e6ab
antoniaKnowles <antoniabackupmac@gmail.com>
2021-03-27 23:18:47 +0000

    fix flip-after scroll-with knowledge built

--- app/src/main/java/com/example/myapplication/KnowledgeLevel1.java
+++ app/src/main/java/com/example/myapplication/KnowledgeLevel1.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.navigation.fragment.NavHostFragment;
 
 class KnowledgeLevel1 extends Fragment {
 
@@ -18,5 +19,30 @@ class KnowledgeLevel1 extends Fragment {
         return inflater.inflate(R.layout.fragment_knowledge_level1, container, false);
     }
 
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.floatingActionButton2).setOnClickListener(new View.OnClickListener() {//On click of home button
+            @Override
+            public void onClick(View view) {
+
+                NavHostFragment.findNavController(KnowledgeLevel1.this).navigate(R.id.action_knowledge_to_FirstFragment);//Take back to home fragment
+
+
+            }
+        });
+        view.findViewById(R.id.floatingActionButton_help).setOnClickListener(new View.OnClickListener() {//On click of home button
+            @Override
+            public void onClick(View view) {
+
+                NavHostFragment.findNavController(KnowledgeLevel1.this).navigate(R.id.action_knowledgeLevel1_to_knowledge);//Take back to home fragment
+
+
+            }
+        });
+
+
+    }
 
 }
